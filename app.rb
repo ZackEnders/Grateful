2ab3458dd1edaeb341d5e5cad4770fc01434cefb
andre <aharris1727@gmail.com>
2018-02-19 16:27:51 -0500

    contact page added

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'sendgrid-ruby'
+include SendGrid
 
 
 get '/' do 
@@ -20,4 +22,27 @@ end
 get '/menu' do
 	erb :menu
 end
+
+
+get '/contact'  do
+@email = params[:email]
+@subject = params[:subject]
+@content = params[:content]
+
+from = Email.new(email: @email)
+to = Email.new(email: 'aharris1727@gmail.com')
+subject = @subject
+content = Content.new(type: 'text/plain', value: @content)
+mail = Mail.new(from, subject, to, content)
+
+sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+response = sg.client.mail._('send').post(request_body: mail.to_json)
+puts response.status_code
+puts response.body
+puts response.headers
+
+
+erb :thanks
+end	
+
 
